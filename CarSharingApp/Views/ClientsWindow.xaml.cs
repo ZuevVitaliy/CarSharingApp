0227686c044443f977000c96ae57a0bf0e5c6b42
Teacher <Teacher@218-03>
2023-01-12 21:54:32 +0200

    Создание окна "Клиенты" + выделение базового окна

---
+++ CarSharingApp/Views/ClientsWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using CarSharingApp.Models.DataBase.Entities;
+using CarSharingApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CarSharingApp.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientsWindow.xaml
+    /// </summary>
+    public partial class ClientsWindow : Window
+    {
+        private readonly ClientsViewModel _clientsViewModel;
+
+        public ClientsWindow()
+        {
+            InitializeComponent();
+            _clientsViewModel = new ClientsViewModel();
+            DataContext = _clientsViewModel;
+        }
+
+        private void ClientsDataGridSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedClients = ((DataGrid)sender).SelectedItems.Cast<Client>();
+            _clientsViewModel.SelectedClients = new ObservableCollection<Client>(selectedClients);
+        }
+    }
+}
