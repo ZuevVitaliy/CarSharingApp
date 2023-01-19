6a2ad9ca3e38b7372f9b5afdf7f128e2d2ae363b
218 Student <218 Student@218-03>
2023-01-19 19:44:45 +0200

    Миграция: добавил столбец Status в Rent

--- CarSharingApp/Migrations/20230119174357_AlterTable_Rents_AddStatusColumn.Designer.cs
+++ CarSharingApp/Migrations/20230119174357_AlterTable_Rents_AddStatusColumn.Designer.cs
@@ -4,6 +4,7 @@ using CarSharingApp.Models.DataBase;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarSharingApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230119174357_AlterTable_Rents_AddStatusColumn")]
+    partial class AlterTableRentsAddStatusColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
