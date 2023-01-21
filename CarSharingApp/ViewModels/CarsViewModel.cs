d2eb04d2ab2b8a0667c05c1d8eceb4c35f78bcf5
UButton189 <nadys.mokeeva.80@mail.ru>
2023-01-21 18:14:42 +0200

    Изменения

--- CarSharingApp/ViewModels/CarsViewModel.cs
+++ CarSharingApp/ViewModels/CarsViewModel.cs
@@ -107,11 +107,6 @@ namespace CarSharingApp.ViewModels
         }
 
         /// <inheritdoc/>
-        protected override void EditCommand_Execute()
-        {
-            base.EditCommand_Execute();
-            Cars = new ObservableCollection<Car>(Cars);
-        }
 
         /// <inheritdoc/>
         protected override Car SelectedItemExtractor()
